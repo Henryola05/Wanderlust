1380f3180cb4594192b58128b60ac00e34ea7f55
Jegede Olatide <60626256+Henryola05@users.noreply.github.com>
2020-06-05 14:53:55 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,73 @@
+// Foursquare API Info
+const clientId = 'U2FZBIYOJEQK5I2OAEUOKP0A5PXQSFHJXZ1MAAQQHEBKTRHP';
+const clientSecret = 'HEJ5DRFEVLFDVQYZIJOZD1A4TNVUNM2UT4LRLZGEB1XNKDTS';
+const url = 'https://api.foursquare.com/v2/venues/explore?near=';
+
+// OpenWeather Info
+const openWeatherKey = '28730111b56be31745e95789232e292f';
+  const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+// Page Elements
+const $input = $('#city');
+const $submit = $('#button');
+const $destination = $('#destination');
+const $container = $('.container');
+const $venueDivs = [$("#venue1"), $("#venue2"), $("#venue3"), $("#venue4")];
+const $weatherDiv = $("#weather1");
+const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+// Add AJAX functions here:
+const getVenues = async () => {
+  const city =  $input.val();
+  const urlToFetch = `${url}${city}&limit=10&client_id=${clientId}&client_secret=${clientSecret}&v=20200605`;
+  try{
+    const response = await fetch(urlToFetch);
+    if(response.ok){
+      console.log(response);
+      const jsonResponse = await response.json();
+      console.log(jsonResponse);
+    } else {
+      throw new Error('Request failed!');
+    }
+  }
+  catch(error){
+    console.log(error);
+  }
+}
+
+
+
+const getForecast = () => {
+
+}
+
+
+// Render functions
+const renderVenues = (venues) => {
+  $venueDivs.forEach(($venue, index) => {
+    // Add your code here:
+
+    let venueContent = '';
+    $venue.append(venueContent);
+  });
+  $destination.append(`<h2>${venues[0].location.city}</h2>`);
+}
+
+const renderForecast = (day) => {
+  // Add your code here:
+  
+	let weatherContent = '';
+  $weatherDiv.append(weatherContent);
+}
+
+const executeSearch = () => {
+  $venueDivs.forEach(venue => venue.empty());
+  $weatherDiv.empty();
+  $destination.empty();
+  $container.css("visibility", "visible");
+  getVenues()
+  getForecast()
+  return false;
+}
+
+$submit.click(executeSearch)
