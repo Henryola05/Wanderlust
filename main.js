4485f92a32184b1f3ea2ff7be4b85e15594d249e
Jegede Olatide <60626256+Henryola05@users.noreply.github.com>
2020-06-06 03:38:44 -0500

    Update main.js

--- main.js
+++ main.js
@@ -20,16 +20,15 @@ const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Frida
 const getVenues = async () => {
   const city =  $input.val();
   const urlToFetch = `${url}${city}&limit=10&client_id=${clientId}&client_secret=${clientSecret}&v=20200605`;
-  try{
-    const response = await fetch(urlToFetch);
-    if(response.ok){
-      console.log(response);
-      const jsonResponse = await response.json();
-      console.log(jsonResponse);
-    } else {
-      throw new Error('Request failed!');
-    }
+  try {
+  const response = await fetch(urlToFetch);
+  if (response.ok) {
+    const jsonResponse = await response.json();
+    const venues = jsonResponse.response.groups[0].items.map(item => item.venue);
+    console.log(venues);
+    return venues;
   }
+}
   catch(error){
     console.log(error);
   }
@@ -37,7 +36,17 @@ const getVenues = async () => {
 
 
 
-const getForecast = () => {
+const getForecast = async () => {
+  const urlToFetch = weatherUrl + '?&q=' + $input.val() + '&APPID=' + openWeatherKey;
+ try {
+  const response = await fetch(urlToFetch);
+  if (response.ok) {
+    const jsonResponse = await response.json();
+    return jsonResponse;
+  }
+} catch (error) {
+    console.log(error);
+  }
 
 }
 
@@ -45,28 +54,28 @@ const getForecast = () => {
 // Render functions
 const renderVenues = (venues) => {
   $venueDivs.forEach(($venue, index) => {
-    // Add your code here:
-
-    let venueContent = '';
+    const venue = venues[index];
+    const venueIcon = venue.categories[0].icon;
+    const venueImgSrc = `${venueIcon.prefix}bg_64${venueIcon.suffix}`;
+    let venueContent = createVenueHTML(venue.name, venue.location, venueImgSrc);
     $venue.append(venueContent);
   });
   $destination.append(`<h2>${venues[0].location.city}</h2>`);
+  
 }
 
 const renderForecast = (day) => {
-  // Add your code here:
-  
-	let weatherContent = '';
+  const weatherContent = createWeatherHTML(day);
   $weatherDiv.append(weatherContent);
-}
+};
 
 const executeSearch = () => {
   $venueDivs.forEach(venue => venue.empty());
   $weatherDiv.empty();
   $destination.empty();
   $container.css("visibility", "visible");
-  getVenues()
-  getForecast()
+   getVenues().then(venues => renderVenues(venues));
+ getForecast().then(forecast => renderForecast(forecast));
   return false;
 }
 
